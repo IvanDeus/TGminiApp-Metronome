e2fc4f9c86baedf435c39ecbe4f12a74ba32d0a6
IvanDeus <146208093+IvanDeus@users.noreply.github.com>
2025-05-29 06:21:20 +0300

    Update app_cfg.example.py

--- app_cfg.example.py
+++ app_cfg.example.py
@@ -2,16 +2,14 @@
 
 # Telegram Bot Token (get it from @BotFather)
 TELEGRAM_BOT_TOKEN = 'YOUR_BOT_TOKEN_HERE'
-
 # Webhook URL (must be public, e.g., ngrok or real server)
 WEBHOOK_URL = "https://myapp.com/"
-
 # Telegram Channel ID (from RawDataBot or similar)
 CHANNEL_ID = "-100YourChannelIDHere"
-
 # Logging settings
 logfpath = "/var/log/TGminiApp-Metronome.log"
-
+# DB name
+DATABASE = 'users_db.sqlite'
 # Local port for webhook
 bot_lport = 6543
 
